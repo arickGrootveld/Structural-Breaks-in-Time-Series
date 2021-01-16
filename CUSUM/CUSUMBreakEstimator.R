765a49ad30802947b463bceb5abde6674d1e5ebc
grootva <grootva@wwu.edu>
2021-01-16 09:42:21 -0800

    added the CUSUM groups code into the simulation

---
+++ CUSUM/CUSUMBreakEstimator.R
@@ -0,0 +1,39 @@
+CUSUMCalc <- function(simMatrix, critVal=0.0133, longRunVar=1){
+  simDims = dim(simMatrix)
+
+  Cor=0 #Count for the number of correct changes detected
+  Mty=NULL
+  
+  for(simNum in c(1:simDims[1])){
+    Dist <- simMatrix[simNum,] #A normal distribution where the mean changes from 5 to 7 at the 20th observation
+    Mn=0 #setting the maxtype stat to 0
+    k=1 #Set k to 1 since we are checking all k's in the range {1,...,n}
+    Ts=0 #Set the test statistic equal to 0 which we will compare to Mn
+    Mt=0
+    while(k<=length(Dist)){ #While loop runs while k is in the range {1,...,n}
+      Ts<-TestStat(Dist, k/(length(Dist)), length(Dist)) #Setting the teststat equal to Ts for different k's
+      k=k+1 #Adding to k after each iteration to keep the loop going
+      if(Ts>=Mn){ #Comparing Ts to Mn
+        Mn=Ts #If the Ts is bigger than Mn will be the "max" of all the k's tested so far
+      }
+      Mt=MaxType(Mn,longRunVar) #Setting the Max Type equal to Mt
+    }
+    if(Mt>critVal){ #Comparing the Max Type to Asymptotic value
+      Cor=Cor+1 #Counting this as a change we detected
+    }
+    Mty=c(Mty, MaxType(Mn,longRunVar))
+  }
+  coverageProbability <- Cor/simDims[1]
+  # Just returning the coverage probabilitiy for right now, may want to return other stuff later as well
+  return(coverageProbability)
+}
+
+# Moved function declarations outside of loops
+# Should probably talk to the group about this
+TestStat<-function(y,x,n){ #Test Statistic (Zn) from Aue and ALexander
+  (1/(sqrt(n)))*((sum(y[c(1,floor(n*x))]))-(((floor(n*x))/(n))*(sum(y[c(1,n)]))))
+}
+
+MaxType=function(m,w){ #Max Type Function from Aue Alexander
+  (1/w)*m
+}
--- runNormalIndepDataProcessAndGatherResults.R
+++ runNormalIndepDataProcessAndGatherResults.R
@@ -3,17 +3,24 @@
 # Importing datagen function
 source('indepDatagen.R')
 
+# Importing CUSUM calculation function
+source('CUSUM/CUSUMBreakEstimator.R')
+
 # Initializing variables
 
-## Arrays to store results
-nEquals50 = c(0,0,0,0)
-nEquals100 = c(0,0,0,0)
-nEquals200 = c(0,0,0,0)
+## Arrays to store results of likelihood ratio calculation
+nEquals50LR = c(0,0,0,0)
+nEquals100LR = c(0,0,0,0)
+nEquals200LR = c(0,0,0,0)
 
-## Parameters for the simulation
+nEquals50CS = c(0,0,0,0)
+nEquals100CS = c(0,0,0,0)
+nEquals200CS = c(0,0,0,0)
+
+## Parameters for the likelihood ratio simulation 
 param1 = 0.1
-param2 = 1
-numSims = 1000
+param2 = 0.1
+numSims = 100
 
 # Performing simulations
 ################################################################################
@@ -22,33 +29,45 @@ simLen <- 50
 ### K=0.2n
 breakLocs <- 0.2 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals50LR[1] = coverageProbability
 
-nEquals50[1] = coverageProbability
+# Cusum Calc
+nEquals50CS[1] = CUSUMCalc(simMatrix)
 
 
 ### K=0.3n
 breakLocs <- 0.3 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals50LR[2] = coverageProbability
 
-nEquals50[2] = coverageProbability
+# Cusum Calc
+nEquals50CS[2] = CUSUMCalc(simMatrix)
 
 
 ### K=0.5n
 breakLocs <- 0.5 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals50LR[3] = coverageProbability
 
-nEquals50[3] = coverageProbability
+# Cusum Calc
+nEquals50CS[3] = CUSUMCalc(simMatrix)
 
 
 ### K=0.75n
 breakLocs <- 0.75 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals50LR[4] = coverageProbability
 
-nEquals50[4] = coverageProbability
+# Cusum Calc
+nEquals50CS[4] = CUSUMCalc(simMatrix)
 
 
 
@@ -58,35 +77,42 @@ simLen <- 100
 ### K=0.2n
 breakLocs <- 0.2 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals100LR[1] = coverageProbability
 
-nEquals100[1] = coverageProbability
-
+# Cusum Calc
+nEquals100CS[1] = CUSUMCalc(simMatrix)
 
 ### K=0.3n
 breakLocs <- 0.3 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals100LR[2] = coverageProbability
 
-nEquals100[2] = coverageProbability
-
+# Cusum Calc
+nEquals100CS[2] = CUSUMCalc(simMatrix)
 
 ### K=0.5n
 breakLocs <- 0.5 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals100LR[3] = coverageProbability
 
-nEquals100[3] = coverageProbability
-
+# Cusum Calc
+nEquals100CS[3] = CUSUMCalc(simMatrix)
 
 ### K=0.75n
 breakLocs <- 0.75 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals100LR[4] = coverageProbability
 
-nEquals100[4] = coverageProbability
-
-
+# Cusum Calc
+nEquals100CS[4] = CUSUMCalc(simMatrix)
 
 ################################################################################
 ## n = 200
@@ -94,43 +120,68 @@ simLen <- 200
 ### K=0.2n
 breakLocs <- 0.2 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals200LR[1] = coverageProbability
 
-nEquals200[1] = coverageProbability
-
+# Cusum Calc
+nEquals200CS[1] = CUSUMCalc(simMatrix)
 
 ### K=0.3n
 breakLocs <- 0.3 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals200LR[2] = coverageProbability
 
-nEquals200[2] = coverageProbability
-
+# Cusum Calc
+nEquals200CS[2] = CUSUMCalc(simMatrix)
 
 ### K=0.5n
 breakLocs <- 0.5 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals200LR[3] = coverageProbability
 
-nEquals200[3] = coverageProbability
-
+# Cusum Calc
+nEquals200CS[3] = CUSUMCalc(simMatrix)
 
 ### K=0.75n
 breakLocs <- 0.75 * simLen
 simMatrix <- indepDatagen(simLen=simLen, numSims=numSims, breakLocations=breakLocs, param1=param1, param2=param2)
-source('normalDataParametricLikelihood.R')
+# LR Calculation
+source('LikelihoodRatio/normalDataParametricLikelihood.R')
+nEquals200LR[4] = coverageProbability
 
-nEquals200[4] = coverageProbability
-
+# Cusum Calc
+nEquals200CS[4] = CUSUMCalc(simMatrix)
 
 
 ################################################################################
-# Constructing the output table
-results.data <- data.frame(
+# Constructing the output table for Likelihood Ratio
+results.dataLR <- data.frame(
   breakLocs = c("K=0.2n", "K=0.3n", "K=0.5n", "K=0.75n"),
-  nEquals50 = nEquals50,
-  nEquals100 = nEquals100,
-  nEquals200 = nEquals200
+  nEquals50 = nEquals50LR,
+  nEquals100 = nEquals100LR,
+  nEquals200 = nEquals200LR
 )
 
-print(results.data)
+results.dataCS <- data.frame(
+  breakLocs = c("K=0.2n", "K=0.3n", "K=0.5n", "K=0.75n"),
+  nEquals50 = nEquals50CS,
+  nEquals100 = nEquals100CS,
+  nEquals200 = nEquals200CS
+)
+
+
+print('Results for the Likelihood ratio')
+print(results.dataLR)
+
+# Printing new line separator
+writeLines('\r\n')
+
+print("Results for the CUSUM calculation")
+print(results.dataCS)
+
+
